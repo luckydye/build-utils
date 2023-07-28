29586a3d1b65d8dbbb196ecbf2e59ffbe7f159f9
Tim Havlicek <t.havlicek@s-v.de>
2023-07-28 17:25:21 +0200

    debian works

--- scripts/build-publish.sh
+++ scripts/build-publish.sh
@@ -1,2 +1,2 @@
 docker buildx build ./docker/debian -t luckydye/build-utils:debian --push
-docker buildx build ./docker/alpine -t luckydye/build-utils:alpine --push
+docker buildx build ./docker/debian -t luckydye/build-utils:latest --push
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,1 +1,3 @@
-docker build test/node -t test/node --progress=plain --no-cache
+docker buildx build ./docker/debian -t luckydye/build-utils:debian
+docker build test/node -t test/node
+docker run -it --entrypoint="bash" test/node
