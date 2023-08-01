f21b79a4bea408c26b0c6aa5ac0ca506a5a6292f
Tim Havlicek <t.havlicek@s-v.de>
2023-08-01 23:27:15 +0200

    ubuntu

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,3 @@
-docker buildx build ./docker/debian -t luckydye/build-utils:debian
+docker buildx build ./docker/ubuntu -t luckydye/build-utils:latest
 docker build test/node -t test/node
 docker run -it --entrypoint="bash" test/node
